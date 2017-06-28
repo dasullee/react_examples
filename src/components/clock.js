c1fba367824805a36b2656765893f373a74a1f61
Scott Ninja Bowler <scott.bowler@learningfuze.com>
2017-06-28 16:15:51 -0700

    Component state example completed

---
+++ src/components/clock.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Clock extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            time: new Date()
+        }
+    }
+
+    componentWillMount(){
+        // Call the tick method every one second
+        setInterval(() => this.tick(), 1000)
+    }
+
+    tick(){
+        // Update state.time to a new time object
+        this.setState({
+            time: new Date()
+        });
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>The time is: {this.state.time.toLocaleTimeString()}</h1>
+            </div>
+        )
+    }
+}
+
+export default Clock;
