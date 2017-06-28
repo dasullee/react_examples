37b5672976c4ce29b1d94ac490a4f5848f2f8abe
Scott Ninja Bowler <scott.bowler@learningfuze.com>
2017-06-28 12:22:56 -0700

    Finished react components slide example

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './app.css';
 import logo from './imgs/logo.svg';
+import Greeting from './greeting';
 
 const App = () => (
     <div>
         <div className="app">
             <img src={logo} className="rotate"/>
             <h1>Welcome to React!</h1>
+            <Greeting name="Bob"/>
         </div>
     </div>
 );
