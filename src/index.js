809379f60753a839f0191e7e46c4ca07d969fe17
Scott Ninja Bowler <scott.bowler@learningfuze.com>
2017-06-28 10:53:37 -0700

    React intro slides complete

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/app';
+//import App from './components/app';
+
+function greeting(user){
+    return `${user.first_name} ${user.last_name}`;
+}
+
+const user = {
+    first_name: 'Dave',
+    last_name: 'Johns'
+}
+
+const greet = <h1>{`Welcome to our site, ${greeting(user)}`}</h1>
 
 ReactDOM.render(
-    <App />,
+    greet,
     document.getElementById('root')
 );
